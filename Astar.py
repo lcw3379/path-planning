bd3d6e484a25eb8ba44f5711c9af29f229db059b
lcw3379 <127228208+lcw3379@users.noreply.github.com>
2024-08-01 12:04:57 +0900

    Update Astar.py

--- Astar.py
+++ Astar.py
@@ -3,6 +3,29 @@ import pygame
 import heapq
 import time
 
+
+start_pos = (0,7)
+end_pos = (13,7)
+maze = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+]
+
+    
+
 class Node:
     def __init__(self, position, parent = None):
         self.position = position
@@ -59,94 +82,91 @@ def Astar(maze, start_pos, end_pos, func):
                     neighbor.g = present_node.g + 10
                 
                 neighbor.h = h_func(neighbor, end_node)
-                neighbor.f = neighbor.g + neighbor.h         
+                neighbor.f = neighbor.g + neighbor.h  
+                if any(o_node for o_node in open if neighbor == o_node and neighbor.g > o_node.g):
+                    continue       
                 
                 heapq.heappush(open, neighbor)       
                 
                 
     
-def make_path(maze, start_pos, end_pos):
 
-    pygame.init()
-    grid_size = 40
-    total_width = len(maze[0])
-    total_height = len(maze)
-    screen = pygame.display.set_mode((total_width * grid_size, total_height * grid_size))
-    pygame.display.set_caption("A*")
-    font = pygame.font.Font(None, 24)
-    font_gh = pygame.font.Font(None, 14)    
-    colors = {
-        "start": (255, 0, 0),
-        "end": (0, 255, 0),
-        "path": (0, 122, 255),
-        "wall": (0, 0, 0),
-        "empty": (255, 255, 255),
-        "open": (0, 255, 255),
-        "present": (255, 0, 255),
-        "closed": (255, 255, 0)
-    }
+
+pygame.init()
+grid_size = 40
+total_width = len(maze[0])
+total_height = len(maze)
+screen = pygame.display.set_mode((total_width * grid_size, total_height * grid_size))
+pygame.display.set_caption("A*")
+font = pygame.font.Font(None, 24)
+font_gh = pygame.font.Font(None, 14)    
+colors = {
+    "start": (255, 0, 0),
+    "end": (0, 255, 0),
+    "path": (0, 122, 255),
+    "wall": (0, 0, 0),
+    "empty": (255, 255, 255),
+    "open": (0, 255, 255),
+    "present": (255, 0, 255),
+    "closed": (255, 255, 0)
+}
+
+def screen_texting(screen, node):
+        text = font_gh.render(str(node.g), True, (0, 0, 0))
+        screen.blit(text, (node.position[1] * grid_size  , node.position[0] * grid_size + 5))            
+        text = font_gh.render(str(node.h), True, (0, 0, 0))
+        screen.blit(text, (node.position[1] * grid_size + 20, node.position[0] * grid_size + 5))            
+        text = font.render(str(node.f), True, (0, 0, 0))
+        screen.blit(text, (node.position[1] * grid_size + 5, node.position[0] * grid_size + 20))    
+
+def visualize(present_node, open, closed_set):
+    for y in range(total_height):
+        for x in range(total_width):
+            if maze[y][x] == 1:
+                color = colors["wall"]
+            else:
+                color = colors["empty"]
+            pygame.draw.rect(screen, color, pygame.Rect(x * grid_size, y * grid_size, grid_size, grid_size))
+    for node in closed_set:
+        pygame.draw.rect(screen, colors["closed"], pygame.Rect(node.position[1] * grid_size, node.position[0] * grid_size, grid_size, grid_size))
+        screen_texting(screen, node)
     
-    def screen_texting(screen, node):
-            text = font_gh.render(str(node.g), True, (0, 0, 0))
-            screen.blit(text, (node.position[1] * grid_size  , node.position[0] * grid_size + 5))            
-            text = font_gh.render(str(node.h), True, (0, 0, 0))
-            screen.blit(text, (node.position[1] * grid_size + 20, node.position[0] * grid_size + 5))            
-            text = font.render(str(node.f), True, (0, 0, 0))
-            screen.blit(text, (node.position[1] * grid_size + 5, node.position[0] * grid_size + 20))    
+    for node in open:
+        pygame.draw.rect(screen, colors["open"], pygame.Rect(node.position[1] * grid_size, node.position[0] * grid_size, grid_size, grid_size))
+        screen_texting(screen, node)
     
-    def visualize(present_node, open, closed_set):
-        for y in range(total_height):
-            for x in range(total_width):
-                if maze[y][x] == 1:
-                    color = colors["wall"]
-                else:
-                    color = colors["empty"]
-                pygame.draw.rect(screen, color, pygame.Rect(x * grid_size, y * grid_size, grid_size, grid_size))
-        for node in closed_set:
-            pygame.draw.rect(screen, colors["closed"], pygame.Rect(node.position[1] * grid_size, node.position[0] * grid_size, grid_size, grid_size))
-            screen_texting(screen, node)
+    pygame.draw.rect(screen, colors["present"], pygame.Rect(present_node.position[1] * grid_size, present_node.position[0] * grid_size, grid_size, grid_size))
+    pygame.draw.rect(screen, colors["start"], pygame.Rect(start_pos[1] * grid_size, start_pos[0] * grid_size, grid_size, grid_size))
+    pygame.draw.rect(screen, colors["end"], pygame.Rect(end_pos[1] * grid_size, end_pos[0] * grid_size, grid_size, grid_size))
+    pygame.display.flip()
+    time.sleep(0.1)       
+
+
+
+path = Astar(maze, start_pos, end_pos, visualize)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            break
         
-        for node in open:
-            pygame.draw.rect(screen, colors["open"], pygame.Rect(node.position[1] * grid_size, node.position[0] * grid_size, grid_size, grid_size))
-            screen_texting(screen, node)
-        
-        pygame.draw.rect(screen, colors["present"], pygame.Rect(present_node.position[1] * grid_size, present_node.position[0] * grid_size, grid_size, grid_size))
-        pygame.draw.rect(screen, colors["start"], pygame.Rect(start_pos[1] * grid_size, start_pos[0] * grid_size, grid_size, grid_size))
-        pygame.draw.rect(screen, colors["end"], pygame.Rect(end_pos[1] * grid_size, end_pos[0] * grid_size, grid_size, grid_size))
-        pygame.display.flip()
-        time.sleep(0.1)       
-    
-    
-    
-    path = Astar(maze, start_pos, end_pos, visualize)
-    
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                break
+    #맵 생성
+    for y in range(total_height):
+        for x in range(total_width):
+            if maze[y][x] == 1:
+                color = colors["wall"]
+            else:
+                color = colors["empty"]
+            pygame.draw.rect(screen, color, pygame.Rect(x * grid_size, y * grid_size, grid_size, grid_size))
             
-        #맵 생성
-        for y in range(total_height):
-            for x in range(total_width):
-                if maze[y][x] == 1:
-                    color = colors["wall"]
-                else:
-                    color = colors["empty"]
-                pygame.draw.rect(screen, color, pygame.Rect(x * grid_size, y * grid_size, grid_size, grid_size))
-                
-        for node in path:
-            pygame.draw.rect(screen, colors["path"], pygame.Rect(node.position[1] * grid_size, node.position[0] * grid_size, grid_size, grid_size))
-            text = font_gh.render(str(node.g), True, (0, 0, 0))
-            screen.blit(text, (node.position[1] * grid_size  , node.position[0] * grid_size + 5))            
-            text = font_gh.render(str(node.h), True, (0, 0, 0))
-            screen.blit(text, (node.position[1] * grid_size + 20, node.position[0] * grid_size + 5))            
-            text = font.render(str(node.f), True, (0, 0, 0))
-            screen.blit(text, (node.position[1] * grid_size + 5, node.position[0] * grid_size + 20))                
-        pygame.draw.rect(screen, colors["start"], pygame.Rect(start_pos[1] * grid_size, start_pos[0] * grid_size, grid_size, grid_size))
-        pygame.draw.rect(screen, colors["end"], pygame.Rect(end_pos[1] * grid_size, end_pos[0] * grid_size, grid_size, grid_size))
-        
-        pygame.display.flip()
+    for node in path:
+        pygame.draw.rect(screen, colors["path"], pygame.Rect(node.position[1] * grid_size, node.position[0] * grid_size, grid_size, grid_size))
+        screen_texting(screen, node) 
+    pygame.draw.rect(screen, colors["start"], pygame.Rect(start_pos[1] * grid_size, start_pos[0] * grid_size, grid_size, grid_size))
+    pygame.draw.rect(screen, colors["end"], pygame.Rect(end_pos[1] * grid_size, end_pos[0] * grid_size, grid_size, grid_size))
+    
+    pygame.display.flip()
     
             
 
@@ -165,25 +185,3 @@ def make_path(maze, start_pos, end_pos):
 #     [0,0,1,0,1,0,0,0],
 #     [0,0,0,0,1,0,0,0],
 # ]    
-maze = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-
-]
-start_pos = (0,7)
-end_pos = (13,7)
-    
-    
-make_path(maze, start_pos, end_pos)
